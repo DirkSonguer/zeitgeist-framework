c6d9db31f835517de202d4d48add015d3d35e531
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2007-11-22 07:03:59 +0000

    fixed loader file

--- zeitgeist-framework/zeitgeist_framework/zeitgeist/zeitgeist.php
+++ zeitgeist-framework/zeitgeist_framework/zeitgeist/zeitgeist.php
@@ -35,7 +35,6 @@
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/session.class.php');
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/userdata.class.php');
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/userrights.class.php');
-	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/usercharacters.class.php');
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/userhandler.class.php');
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/errorhandler.class.php');
 	require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/template.class.php');
