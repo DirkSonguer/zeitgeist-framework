67b53592460573a079a4327f7215ddf120a7f0d3
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2007-12-21 09:14:18 +0000

    basic stuff

---
+++ zeitgeist/applications/lineracer/modules/main/main.module.php
@@ -0,0 +1,92 @@
+<?php
+
+
+defined('LINERACER_ACTIVE') or die();
+
+class main
+{
+	protected $debug;
+	protected $messages;
+	protected $database;
+	protected $configuration;
+	protected $user;
+
+	public function __construct()
+	{
+		$this->debug = zgDebug::init();
+		$this->messages = zgMessages::init();
+		$this->configuration = zgConfiguration::init();
+		$this->user = zgUserhandler::init();
+		
+		$this->database = new zgDatabase();
+		$this->database->connect();
+	}
+	
+	
+	public function index($parameters=array())
+	{
+		$this->debug->guard();
+		
+		$tpl = new zgaTemplate();
+		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_index'));
+		
+		$tpl->show();
+		
+		$this->debug->unguard(true);
+		return true;
+	}
+	
+	
+	public function login($parameters=array())
+	{
+		$this->debug->guard();
+		
+		$tpl = new lrTemplate();
+		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_login'));
+		
+		if ($this->user->isLoggedIn())
+		{
+			$tpl->redirect($tpl->createLink('main', 'index'));
+		}
+				
+		if (!empty($parameters['login']))
+		{
+			if ( (!empty($parameters['username'])) && (!empty($parameters['password'])) )
+			{
+				if ($this->user->loginUser($parameters['username'], $parameters['password']))
+				{
+					$tpl->redirect($tpl->createLink('main', 'index'));
+				}
+				else
+				{
+					$this->messages->setMessage('Username and/or password was not correct. Please enter your username and password carefully.', 'userwarning');
+				}
+			}
+			else
+			{
+				$this->messages->setMessage('Please enter a valid username and password.', 'userwarning');
+			}
+		}
+				
+		$tpl->show();
+				
+		$this->debug->unguard(true);
+		return true;
+	}
+
+	
+	public function logout($parameters=array())
+	{
+		$this->debug->guard();
+		
+		$this->user->logoutUser();
+		
+		$tpl = new lrTemplate();
+		$tpl->redirect($tpl->createLink('main', 'index'));
+				
+		$this->debug->unguard(true);
+		return true;
+	}
+
+}
+?>
