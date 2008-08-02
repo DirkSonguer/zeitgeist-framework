374f9c8456fa92529ccd3c16b16a01b82bacbd6a
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2008-08-02 09:13:13 +0000

    configuration test done database test done

--- zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/index.php
+++ zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/index.php
@@ -10,14 +10,22 @@
 	if (!defined('ZEITGEIST_ROOTDIRECTORY')) define('ZEITGEIST_ROOTDIRECTORY', '../');
 	require_once('../zeitgeist.php');
 
+	define('ZG_DB_DBSERVER', 'localhost');
+	define('ZG_DB_USERNAME', 'root');
+	define('ZG_DB_USERPASS', '');
+	define('ZG_DB_DATABASE', 'zg_test');
+	define('ZG_DB_CONFIGURATIONCACHE', 'configurationcache');
+	
 	require_once('messages.test.php');
 	require_once('database.test.php');
+	require_once('configuration.test.php');
 
 	$debug = zgDebug::init();
 
     $test = &new TestSuite('Zeitgeist Unit Tests');
     $test->addTestCase(new testMessages());
     $test->addTestCase(new testDatabase());
+    $test->addTestCase(new testConfiguration());
     $test->run(new HtmlReporter());
 
 	$debug->loadStylesheet('debug.css');
