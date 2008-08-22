d2e788ae3384f5915bc812c199f5cfe695c71409
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2008-08-22 05:02:54 +0000

    initial configuration and setup

---
+++ zeitgeist/applications/wellnesswelt/configuration/wellnesswelt.config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('WELLNESSWELT_ACTIVE') or die();
+
+	define('ZG_DB_DBSERVER', 'localhost');
+	define('ZG_DB_USERNAME', 'root');
+	define('ZG_DB_USERPASS', '');
+	define('ZG_DB_DATABASE', 'wellnesswelt');
+	define('ZG_DB_CONFIGURATIONCACHE', 'configurationcache');
+
+?>
