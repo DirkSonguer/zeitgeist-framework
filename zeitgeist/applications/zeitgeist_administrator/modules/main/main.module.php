d73e6e700905bd4f651cd55b9c6c2f73629a2ef2
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2008-09-21 18:34:14 +0000

    more test cases template work

--- zeitgeist/applications/zeitgeist_administrator/modules/main/main.module.php
+++ zeitgeist/applications/zeitgeist_administrator/modules/main/main.module.php
@@ -17,43 +17,43 @@ class main
 		$this->messages = zgMessages::init();
 		$this->configuration = zgConfiguration::init();
 		$this->user = zgUserhandler::init();
-		
+
 		$this->database = new zgDatabase();
 		$this->database->connect();
 	}
-	
-	
+
+
 	public function index($parameters=array())
 	{
 		$this->debug->guard();
-		
+
 		$tpl = new zgaTemplate();
 		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_index'));
-		
+
 		$tpl->show();
-		
+
 		$this->debug->unguard(true);
 		return true;
 	}
-	
-	
+
+
 	public function login($parameters=array())
 	{
 		$this->debug->guard();
-		
+
 		$tpl = new zgaTemplate();
 		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_login'));
-		
+
 		if ($this->user->isLoggedIn())
 		{
 			$tpl->redirect($tpl->createLink('main', 'index'));
 		}
-				
+
 		if (!empty($parameters['login']))
 		{
 			if ( (!empty($parameters['username'])) && (!empty($parameters['password'])) )
 			{
-				if ($this->user->loginUser($parameters['username'], $parameters['password']))
+				if ($this->user->login($parameters['username'], $parameters['password']))
 				{
 					$tpl->redirect($tpl->createLink('main', 'index'));
 				}
@@ -67,23 +67,23 @@ class main
 				$this->messages->setMessage('Please enter a valid username and password.', 'userwarning');
 			}
 		}
-				
+
 		$tpl->show();
-				
+
 		$this->debug->unguard(true);
 		return true;
 	}
 
-	
+
 	public function logout($parameters=array())
 	{
 		$this->debug->guard();
-		
-		$this->user->logoutUser();
-		
+
+		$this->user->logout();
+
 		$tpl = new zgaTemplate();
 		$tpl->redirect($tpl->createLink('main', 'index'));
-				
+
 		$this->debug->unguard(true);
 		return true;
 	}
