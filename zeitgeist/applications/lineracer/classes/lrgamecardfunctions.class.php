40bfbd3f0aa152101b96ef1e40b44f19446b3f32
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2008-10-17 19:24:31 +0000

    started adding unit testing to lineracer classes

--- zeitgeist/applications/lineracer/classes/lrgamecardfunctions.class.php
+++ zeitgeist/applications/lineracer/classes/lrgamecardfunctions.class.php
@@ -23,6 +23,56 @@ class lrGamecardfunctions
 		$this->database->connect();
 	}
 
+
+	public function playGamecard($gamecard)
+	{
+		$this->debug->guard();
+		
+		if ( (!$currentGamestates = $this->objects->getObject('currentGamestates')) )
+		{
+			$this->debug->write('Could not play gamecard: gamestates are not loaded', 'warning');
+			$this->messages->setMessage('Could not play gamecard: gamestates are not loaded', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+		
+		if (!$this->validateTurn())
+		{
+			$this->debug->write('Could not play gamecard: it is another players turn', 'warning');
+			$this->messages->setMessage('Could not play gamecard: it is another players turn', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+		
+		$gamecardfunctions = new lrGamecardfunctions();
+		if (!$gamecardfunctions->checkRights($gamecard))
+		{
+			$this->debug->write('Could not play gamecard: no rights to play gamecard', 'warning');
+			$this->messages->setMessage('Could not play gamecard: no rights to play gamecard', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		if (!$gamecardData = $gamecardfunctions->getGamecardData($gamecard))
+		{
+			$this->debug->write('Could not play gamecard: gamecard not found', 'warning');
+			$this->messages->setMessage('Could not play gamecard: gamecard not found', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		// TODO: 3 = gamecard. use constant
+		$this->_saveGamestates('3', $gamecard);
+		
+		// TODO: player, round
+		$gameeventhandler = new lrGameeventhandler();
+		$gameeventhandler->saveRaceevent('1', $gamecard, $currentGamestates['activePlayer']+$gamecardData['gamecard_roundoffset']);
+		$gamecardfunctions->redeemGamecard($gamecard);
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
 	
 	// TODO: Richitg aufsetzen
 	public function getGamecardData($gamecard)
@@ -73,21 +123,94 @@ class lrGamecardfunctions
 	}
 
 	// TODO: user Abfrage
-	public function redeemGamecard($gamecard, $user=0)
+	public function removeGamecard($gamecard, $user)
 	{
 		$this->debug->guard();
 
-		$sql = "DELETE FROM users_to_gamecards WHERE usergamecard_gamecard='" . $gamecard . "' AND usergamecard_user='" . $user . "'";
+		$sql = "SELECT * FROM users_to_gamecards WHERE usergamecard_gamecard='" . $gamecard . "' AND usergamecard_user='" . $user . "'";
 		$res = $this->database->query($sql);
 		if (!$res)
 		{
-			$this->debug->write('Right denied: could not find gamecard in user deck', 'warning');
-			$this->messages->setMessage('Right denied: could not find gamecard in user deck', 'warning');
+			$this->debug->write('Could not remove gamecard: could not remove gamecard values from the database', 'warning');
+			$this->messages->setMessage('Could not remove gamecard: could not remove gamecard values from the database', 'warning');
 			$this->debug->unguard(false);
 			return false;
 		}
 
+		$count = $this->database->numRows($res);
+		
+		if ($count == 0)
+		{
+			$this->debug->write('Gamecard to delete was not found. Possible problem with interface', 'warning');
+			$this->messages->setMessage('Gamecard to delete was not found. Possible problem with interface', 'warning');
+			$this->debug->unguard(true);
+			return true;
+		}
+			
 		$row = $this->database->fetchArray($res);
+		$count = $row['usergamecard_count'];
+			
+			$sql = "DELETE FROM users_to_gamecards(usergamecard_gamecard, usergamecard_user, usergamecard_count) VALUES('" . $gamecard . "', '" . $user . "', '1')";
+			$res = $this->database->query($sql);
+			if (!$res)
+			{
+				$this->debug->write('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->messages->setMessage('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->debug->unguard(false);
+				return false;
+			}
+		}
+		else
+		{
+			$sql = "UPDATE users_to_gamecards SET usergamecard_gamecard='" . $gamecard . "', usergamecard_user='" . $user . "', usergamecard_count='" . ($count+1) . "' WHERE usergamecard_gamecard='" . $gamecard . "' AND usergamecard_user='" . $user . "'";
+			$res = $this->database->query($sql);
+			if (!$res)
+			{
+				$this->debug->write('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->messages->setMessage('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->debug->unguard(false);
+				return false;
+			}		
+		}
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	// TODO: user Abfrage
+	public function addGamecard($gamecard, $user)
+	{
+		$this->debug->guard();
+
+		$sql = "SELECT * FROM users_to_gamecards WHERE usergamecard_gamecard='" . $gamecard . "' AND usergamecard_user='" . $user . "'";
+		$res = $this->database->query($sql);
+		$count = $this->database->numRows($res);
+		
+		if ($count == 0)
+		{
+			$sql = "INSERT INTO users_to_gamecards(usergamecard_gamecard, usergamecard_user, usergamecard_count) VALUES('" . $gamecard . "', '" . $user . "', '1')";
+			$res = $this->database->query($sql);
+			if (!$res)
+			{
+				$this->debug->write('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->messages->setMessage('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->debug->unguard(false);
+				return false;
+			}
+		}
+		else
+		{
+			$sql = "UPDATE users_to_gamecards SET usergamecard_gamecard='" . $gamecard . "', usergamecard_user='" . $user . "', usergamecard_count='" . ($count+1) . "' WHERE usergamecard_gamecard='" . $gamecard . "' AND usergamecard_user='" . $user . "'";
+			$res = $this->database->query($sql);
+			if (!$res)
+			{
+				$this->debug->write('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->messages->setMessage('Could not add gamecard: could not insert gamecard values into the database', 'warning');
+				$this->debug->unguard(false);
+				return false;
+			}		
+		}
 
 		$this->debug->unguard(true);
 		return true;
