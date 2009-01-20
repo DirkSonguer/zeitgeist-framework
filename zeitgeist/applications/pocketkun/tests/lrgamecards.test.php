f1ac5a185a98f0902f4ec076284cd2823dc39f10
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2009-01-20 21:48:54 +0000

    initializing project

---
+++ zeitgeist/applications/pocketkun/tests/lrgamecards.test.php
@@ -0,0 +1,66 @@
+<?php
+
+class testLrgamecards extends UnitTestCase
+{
+	public $database;
+	public $miscfunctions;
+	
+	function test_init()
+	{
+		$this->database = new zgDatabase();
+		$this->database->connect();
+		$this->miscfunctions  = new miscFunctions();
+
+		$gameeventhandler = new lrGameeventhandler();
+		$this->assertNotNull($gameeventhandler);
+		unset($gameeventhandler);
+    }
+
+	function test_dash()
+	{
+		$gamestates = new lrGamestates();
+		$gameeventhandler = new lrGameeventhandler();
+		$objects = zgObjects::init();
+		$configuration = zgConfiguration::init();
+
+		$this->miscfunctions->setupGame();
+
+		$gamestates->loadGamestates(1);
+		$currentGamestates = $objects->getObject('currentGamestates');
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][0], 5);
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][1], 10);
+
+		$gameeventhandler->saveRaceevent('1', $configuration->getConfiguration('gamedefinitions', 'events', 'playgamecard'), '2');
+		$objects->deleteObject('currentGamestates');
+
+		$gamestates->loadGamestates(1);
+		$currentGamestates = $objects->getObject('currentGamestates');
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][0], 10);
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][1], 20);
+	}
+
+	function test_fullbreak()
+	{
+		$gamestates = new lrGamestates();
+		$gameeventhandler = new lrGameeventhandler();
+		$objects = zgObjects::init();
+		$configuration = zgConfiguration::init();
+
+		$this->miscfunctions->setupGame();
+
+		$gamestates->loadGamestates(1);
+		$currentGamestates = $objects->getObject('currentGamestates');
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][0], 5);
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][1], 10);
+
+		$gameeventhandler->saveRaceevent('1', $configuration->getConfiguration('gamedefinitions', 'events', 'playgamecard'), '3');
+		$objects->deleteObject('currentGamestates');
+
+		$gamestates->loadGamestates(1);
+		$currentGamestates = $objects->getObject('currentGamestates');
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][0], 0);
+		$this->assertEqual($currentGamestates['playerdata'][$currentGamestates['currentPlayer']]['vector'][1], 0);
+	}
+}
+
+?>
