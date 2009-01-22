cbe9ddb777f17a7c19e90c565a8591fdb1fa3bc9
delax <delax@046e039e-1113-11df-9f54-17e475596acb>
2009-01-22 06:28:46 +0000

    started development

--- zeitgeist/applications/pocketkun/modules/main/main.module.php
+++ zeitgeist/applications/pocketkun/modules/main/main.module.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('PROJECT_ACTIVE') or die();
+defined('POCKETKUN_ACTIVE') or die();
 
 class main
 {
@@ -22,13 +22,74 @@ class main
 	}
 
 
+	// ok
 	public function index($parameters=array())
 	{
 		$this->debug->guard();
 
+		$tpl = new pkTemplate();
+		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_index'));
+
+		$tpl->show();
+
 		$this->debug->unguard(true);
 		return true;
 	}
+
+
+	// ok
+	public function login($parameters=array())
+	{
+		$this->debug->guard();
+
+		$tpl = new pkTemplate();
+
+		if ($this->user->isLoggedIn())
+		{
+			$tpl->redirect($tpl->createLink('main', 'index'));
+		}
+
+		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_login'));
+		if (!empty($parameters['login']))
+		{
+			if ( (!empty($parameters['username'])) && (!empty($parameters['password'])) )
+			{
+				if ($this->user->login($parameters['username'], $parameters['password']))
+				{
+					$tpl->redirect($tpl->createLink('main', 'index'));
+				}
+				else
+				{
+					$this->messages->setMessage('Der Benutzername und/oder das Passwort wurde nicht korrekt angegeben. Bitte geben Sie Ihren Benutzernamen und Ihr Passwort sorgfältig ein.', 'userwarning');
+				}
+			}
+			else
+			{
+				$this->messages->setMessage('Bitte geben Sie einen gültigen Benutzernamen und das dazugehörige Passwort ein.', 'userwarning');
+			}
+		}
+
+		$tpl->show();
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	// ok
+	public function logout($parameters=array())
+	{
+		$this->debug->guard();
+
+		$this->user->logout();
+
+		$tpl = new pkTemplate();
+		$tpl->redirect($tpl->createLink('main', 'index'));
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
 
 }
 ?>
