b1f5dd4bff6250e097ed9b4d882e95f0f33fda8f
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-08-17 20:13:57 +0000

    Initial import of an empty project

---
+++ zeitgeist/framework/zeitgeist_project/index.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Zeitgeist
+ * A PHP based multipurpose framework for web applications
+ * http://www.zeitgeist-framework.com
+ *
+ * @author Dirk Songür <songuer@zeitgeist-framework.com>
+ * 
+ * @copyright http://www.zeitgeist-framework.com
+ * @license http://www.zeitgeist-framework.com/zeitgeist/license.txt
+ * 
+ * @package ZEITGEIST PROJECT
+ * @subpackage ZEITGEIST PROJECT MAIN
+ */
+
+	// set define for application
+	define('APPLICATION_ACTIVE', true);
+	
+	// activate debugging
+	define('DEBUGMODE', true);
+
+	// require basic configuration
+	require_once('configuration/application.configuration.php');	
+	
+	// define zeitgeist specific path values
+	if (!defined('ZEITGEIST_ROOTDIRECTORY')) define('ZEITGEIST_ROOTDIRECTORY', './zeitgeist/');
+	if (!defined('APPLICATION_ROOTDIRECTORY')) define('APPLICATION_ROOTDIRECTORY', './');
+
+	// include framework
+	include('zeitgeist/zeitgeist.php');
+	
+	// define some general classes
+	$debug = zgDebug::init();
+	$session = zgSession::init();
+	$message = zgMessages::init();
+	$configuration = zgConfiguration::init();
+	$error = zgErrorhandler::init();
+	
+	// define a user and bind him to the application pipeline
+	$user = zgUserhandler::init();
+	$eventhandler = new zgEventhandler();
+	$eventhandler->init($user);
+	
+	// load application configuration
+	$configuration->loadConfiguration('application', 'configuration/application.ini');
+
+	// set module to input or defult value
+	if (isset($_GET['module']))
+	{
+		$module = $_GET['module'];
+	}
+	else
+	{
+		$module = 'main';
+	}
+	
+	// set action to input or defult value
+	if (isset($_GET['action']))
+	{
+		$action = $_GET['action'];
+	}
+	else
+	{
+		$action = 'index';
+	}
+
+	// establish user session if possible
+	$activesession = $user->establishUserSession();
+
+	// hand over to applicaton pipeline
+	$ret = $eventhandler->callEvent($module, $action);
+
+	// output all debug information
+	$debug->loadStylesheet('debug.css');
+	$debug->showInnerLoops = true;
+	$debug->showMiscInformation();
+	$debug->showDebugMessages();
+	$debug->showQueryMessages();
+	$debug->showGuardMessages();
+
+?>
+
