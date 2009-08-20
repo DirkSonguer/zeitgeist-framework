0c691fc8a77293ce60965f943646a1e85076bc48
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-08-20 20:56:12 +0000

    Added setup overview page small template changes

--- zeitgeist/framework/zeitgeist_administrator_v2/modules/setup/setup.module.php
+++ zeitgeist/framework/zeitgeist_administrator_v2/modules/setup/setup.module.php
@@ -2,7 +2,7 @@
 
 defined('ZGADMIN_ACTIVE') or die();
 
-class main
+class setup
 {
 	protected $debug;
 	protected $messages;
@@ -29,60 +29,9 @@ class main
 		$this->debug->guard();
 		
 		$tpl = new zgaTemplate();
-		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_index'));
+		$tpl->load($this->configuration->getConfiguration('setup', 'templates', 'setup_index'));
 
 		$tpl->show();
-
-		$this->debug->unguard(true);
-		return true;
-	}
-
-
-	public function login($parameters=array())
-	{
-		$this->debug->guard();
-
-		$tpl = new zgaTemplate();
-		$tpl->load($this->configuration->getConfiguration('main', 'templates', 'main_login'));
-
-		if ($this->user->isLoggedIn())
-		{
-			$tpl->redirect($tpl->createLink('main', 'index'));
-		}
-
-		if (!empty($parameters['login']))
-		{
-			if ( (!empty($parameters['username'])) && (!empty($parameters['password'])) )
-			{
-				if ($this->user->login($parameters['username'], $parameters['password']))
-				{
-					$tpl->redirect($tpl->createLink('main', 'index'));
-				}
-				else
-				{
-					$this->messages->setMessage('Username and/or password was not correct. Please enter your username and password carefully.', 'userwarning');
-				}
-			}
-			else
-			{
-				$this->messages->setMessage('Please enter a valid username and password.', 'userwarning');
-			}
-		}
-
-		$tpl->show();
-
-		$this->debug->unguard(true);
-		return true;
-	}
-
-	public function logout($parameters=array())
-	{
-		$this->debug->guard();
-
-		$this->user->logout();
-
-		$tpl = new zgaTemplate();
-		$tpl->redirect($tpl->createLink('main', 'index'));
 
 		$this->debug->unguard(true);
 		return true;
