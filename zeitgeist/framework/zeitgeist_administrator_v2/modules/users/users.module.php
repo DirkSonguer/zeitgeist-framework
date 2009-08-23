60227b142c63eec1fa50646ff28e9a5e6b222047
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-08-23 11:59:23 +0000

    started users module fixed bug in css new users module

---
+++ zeitgeist/framework/zeitgeist_administrator_v2/modules/users/users.module.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('ZGADMIN_ACTIVE') or die();
+
+class users
+{
+	protected $debug;
+	protected $messages;
+	protected $messagecache;
+	protected $database;
+	protected $configuration;
+	protected $user;
+
+	public function __construct()
+	{
+		$this->debug = zgDebug::init();
+		$this->messages = zgMessages::init();
+		$this->messagecache = zgMessagecache::init();
+		$this->configuration = zgConfiguration::init();
+		$this->user = zgUserhandler::init();
+
+		$this->database = new zgDatabase();
+		$this->database->connect();
+	}
+
+
+	public function index($parameters=array())
+	{
+		$this->debug->guard();
+		
+		$tpl = new zgaTemplate();
+		$tpl->load($this->configuration->getConfiguration('users', 'templates', 'users_index'));
+
+		$tpl->show();
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+}
+?>
