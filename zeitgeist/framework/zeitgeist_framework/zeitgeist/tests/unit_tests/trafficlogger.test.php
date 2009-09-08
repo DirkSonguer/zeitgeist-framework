5552e30de86d8269a0d2257538da539017137f09
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-09-08 06:04:29 +0000

    Tests for parameterhandler and trafficlogger

--- zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/unit_tests/trafficlogger.test.php
+++ zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/unit_tests/trafficlogger.test.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined('MULTITEST')) include(dirname(__FILE__).'/../test_configuration.php');
+
 class testTrafficlogger extends UnitTestCase
 {
 
@@ -15,34 +17,46 @@ class testTrafficlogger extends UnitTestCase
 		unset($trafficlogger);		
     }
 	
-	function test_createUser()
+	
+	// Test logging a pageview
+	function test_logPageview()
 	{
 		$trafficlogger = new zgTrafficlogger();
 		$this->database->query('TRUNCATE TABLE trafficlog');
 		$this->database->query('TRUNCATE TABLE trafficlog_parameters');
 		
+		$param1 = rand(100, 5000);
+		$param2 = rand(100, 5000);
+		$param3 = rand(100, 5000);
+		
 		$parameters = array();
-		$parameters['test1'] = 'test1';
-		$parameters['test2'] = 'test2';
-		$ret = $trafficlogger->logPageview('1', '1', '1', $parameters);
+		$parameters['test1'] = 'test'.$param1;
+		$parameters['test2'] = 'test'.$param2;
+		$ret = $trafficlogger->logPageview($param1, $param2, $param3, $parameters);
 		$this->assertTrue($ret);
 
 		$res = $this->database->query("SELECT * FROM trafficlog");
+
+		// should be only one entry we just entered
 		$ret = $this->database->numRows($res);
 		$this->assertEqual($ret, 1);
+
 		$ret = $this->database->fetchArray($res);
-		$this->assertEqual($ret['trafficlog_module'], '1');
-		$this->assertEqual($ret['trafficlog_action'], '1');
+		$this->assertEqual($ret['trafficlog_module'], $param1);
+		$this->assertEqual($ret['trafficlog_action'], $param2);
+		$this->assertEqual($ret['trafficlog_user'], $param3);
 
 		$res = $this->database->query("SELECT * FROM trafficlog_parameters");
 		$ret = $this->database->numRows($res);
 		$this->assertEqual($ret, 2);
+
 		$ret = $this->database->fetchArray($res);
 		$this->assertEqual($ret['trafficparameters_key'], 'test1');
-		$this->assertEqual($ret['trafficparameters_value'], 'test1');
+		$this->assertEqual($ret['trafficparameters_value'], 'test'.$param1);
+
 		$ret = $this->database->fetchArray($res);
 		$this->assertEqual($ret['trafficparameters_key'], 'test2');
-		$this->assertEqual($ret['trafficparameters_value'], 'test2');
+		$this->assertEqual($ret['trafficparameters_value'], 'test'.$param2);
 
 		unset($trafficlogger);
 	}
