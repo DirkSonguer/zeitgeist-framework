144cdfbad870745bcc0a2fc1147f8d9b39db3cbd
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-09-08 19:49:56 +0000

    more unit tests for the parameter handler

--- zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/unit_tests/parameterhandler.test.php
+++ zeitgeist/framework/zeitgeist_framework/zeitgeist/tests/unit_tests/parameterhandler.test.php
@@ -13,465 +13,199 @@ class testParameterhandler extends UnitTestCase
     }
 
 
-	// Test date parameter
-	function test_getSafeParameters_date()
+	// Test regexp parameter non-escaped with valid parameter
+	function test_getSafeParameters_regexp_valid()
 	{
+		$_GET['test_regexp'] = '1234';
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';						
-
-		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters_GET');
+		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
 		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
+		$this->assertNotNull($ret['test_regexp']);
+		$this->assertEqual($ret['test_regexp'], '1234');
 
+		unset($ret);
 		unset($parameterhandler);
     }
-/*
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test regexp parameter non-escaped with invalid parameter
+	function test_getSafeParameters_regexp_invalid()
 	{
+		$_GET['test_regexp'] = '12';
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNull($ret['test_regexp']);
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test regexp parameter escaped with valid parameter
+	function test_getSafeParameters_regexp_escaped_valid()
 	{
+		$_GET['test_regexp_escaped'] = "12'34";
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNotNull($ret['test_regexp_escaped']);
+		$this->assertEqual($ret['test_regexp_escaped'], "12\'34");
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test regexp parameter escaped with invalid parameter
+	function test_getSafeParameters_regexp_escaped_invalid()
 	{
+		$_GET['test_regexp_escaped'] = '12';
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNull($ret['test_regexp_escaped']);
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test text parameter with valid parameter
+	function test_getSafeParameters_text_valid()
 	{
+		$_GET['test_text'] = "This is a test";
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNotNull($ret['test_text']);
+		$this->assertEqual($ret['test_text'], "This is a test");
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test text parameter with invalid parameter
+	function test_getSafeParameters_text_invalid()
 	{
+		$_GET['test_text'] = "Illegal chars: ';";
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNull($ret['test_text']);
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	
-	// Test string
-	function test_getSafeParameters()
+
+
+	// Test string parameter with valid parameter
+	function test_getSafeParameters_string_valid()
 	{
+		$_GET['test_string'] = "This is a test";
 		$parameterhandler = new zgParameterhandler();
 
 		$configuration = zgConfiguration::init();
 		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
 
-		$_GET['false'] = 'false';
-		$_GET['test_text_true'] = 'Hallo';
-		$_GET['test_text_false'] = '.^"!~';
-		$_GET['test_reg_true'] = '1';
-		$_GET['test_reg_false'] = 'false';
-		$_GET['test_date_true'] = '01.01.1970';
-		$_GET['test_date_false'] = 'false';
-
-		$_POST['false'] = 'false';
-		$_POST['test_text_true'] = 'Hallo';
-		$_POST['test_text_false'] = '.^"!~';
-		$_POST['test_reg_true'] = '1';
-		$_POST['test_reg_false'] = 'false';
-		$_POST['test_date_true'] = '01.01.1970';
-		$_POST['test_date_false'] = 'false';
-						
-
 		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
-		$this->assertNull($ret['"false']);
+		$this->assertNull($ret['false']);
 
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
+		$this->assertNotNull($ret['test_string']);
+		$this->assertEqual($ret['test_string'], "This is a test");
 
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
 		unset($ret);
-
-		$ret = $parameterhandler->getSafeParameters('parameter', 'test_post');
-		$this->assertNull($ret['"false']);
-
-		if ($this->assertNotNull($ret['test_text_true']))
-		{
-			$this->assertEqual($ret['test_text_true'], 'Hallo');
-		}
-		$this->assertNull($ret['"test_text_false']);
-
-		if ($this->assertNotNull($ret['test_reg_true']))
-		{
-			$this->assertEqual($ret['test_reg_true'], '1');
-		}
-		$this->assertNull($ret['test_reg_false']);
-
-		if ($this->assertNotNull($ret['test_date_true']))
-		{
-			$this->assertEqual($ret['test_date_true'], '01.01.1970');
-		}
-		$this->assertNull($ret['"test_date_false']);
-		unset($ret);
-
 		unset($parameterhandler);
     }
-	*/
+
+
+	// Test string parameter with invalid parameter
+	function test_getSafeParameters_string_invalid()
+	{
+		$_GET['test_string'] = "Illegal chars: \n\t";
+		$parameterhandler = new zgParameterhandler();
+
+		$configuration = zgConfiguration::init();
+		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
+
+		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
+		$this->assertNull($ret['false']);
+
+		$this->assertNull($ret['test_string']);
+
+		unset($ret);
+		unset($parameterhandler);
+    }
+
+	// Test date parameter with valid parameter
+	function test_getSafeParameters_date_valid()
+	{
+		$_GET['test_date'] = "01.01.1970";
+		$parameterhandler = new zgParameterhandler();
+
+		$configuration = zgConfiguration::init();
+		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
+
+		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
+		$this->assertNull($ret['false']);
+
+		$this->assertNotNull($ret['test_date']);
+		$this->assertEqual($ret['test_date'], "01.01.1970");
+
+		unset($ret);
+		unset($parameterhandler);
+    }
+
+
+	// Test string parameter escaped with invalid parameter
+	function test_getSafeParameters_date_invalid()
+	{
+		$_GET['test_date'] = "001.001.1970";
+		$parameterhandler = new zgParameterhandler();
+
+		$configuration = zgConfiguration::init();
+		$ret = $configuration->loadConfiguration('testparameters', ZEITGEIST_ROOTDIRECTORY.'tests/testdata/testparameters.ini');
+
+		$ret = $parameterhandler->getSafeParameters('testparameters', 'test_parameters');
+		$this->assertNull($ret['false']);
+
+		$this->assertNull($ret['test_date']);
+
+		unset($ret);
+		unset($parameterhandler);
+    }
+
 }
 
 ?>
