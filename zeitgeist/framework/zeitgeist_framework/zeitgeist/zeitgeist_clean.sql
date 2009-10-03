9dc9d95853d3565fa7e0236dd9641c78caa67fd0
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-10-03 05:33:55 +0000

    Updated Database

--- zeitgeist/framework/zeitgeist_framework/zeitgeist/zeitgeist_clean.sql
+++ zeitgeist/framework/zeitgeist_framework/zeitgeist/zeitgeist_clean.sql
@@ -1,13 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.1
+-- version 3.1.3.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 02. August 2008 um 08:42
--- Server Version: 5.0.45
--- PHP-Version: 5.2.4
+-- Erstellungszeit: 12. September 2009 um 08:29
+-- Server Version: 5.1.33
+-- PHP-Version: 5.2.9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Datenbank: `zg_test`
@@ -19,13 +25,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Tabellenstruktur für Tabelle `actions`
 --
 
-CREATE TABLE `actions` (
-  `action_id` int(12) NOT NULL auto_increment,
-  `action_module` int(12) NOT NULL default '0',
-  `action_name` varchar(30) NOT NULL default '',
+CREATE TABLE IF NOT EXISTS `actions` (
+  `action_id` int(12) NOT NULL AUTO_INCREMENT,
+  `action_module` int(12) NOT NULL DEFAULT '0',
+  `action_name` varchar(30) NOT NULL DEFAULT '',
   `action_description` text NOT NULL,
-  `action_requiresuserright` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`action_id`),
+  `action_requiresuserright` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`action_id`),
   KEY `action_module` (`action_module`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -40,12 +46,12 @@ CREATE TABLE `actions` (
 -- Tabellenstruktur für Tabelle `configurationcache`
 --
 
-CREATE TABLE `configurationcache` (
-  `configurationcache_id` int(12) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `configurationcache` (
+  `configurationcache_id` int(12) NOT NULL AUTO_INCREMENT,
   `configurationcache_name` varchar(128) NOT NULL,
   `configurationcache_timestamp` varchar(32) NOT NULL,
   `configurationcache_content` text NOT NULL,
-  PRIMARY KEY  (`configurationcache_id`),
+  PRIMARY KEY (`configurationcache_id`),
   UNIQUE KEY `configuration_cache_modulename` (`configurationcache_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -57,32 +63,15 @@ CREATE TABLE `configurationcache` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `messagecache`
---
-
-CREATE TABLE `messagecache` (
-  `messagecache_user` int(12) NOT NULL,
-  `messagecache_content` text character set latin1 collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`messagecache_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Daten für Tabelle `messagecache`
---
-
-
--- --------------------------------------------------------
-
---
 -- Tabellenstruktur für Tabelle `modules`
 --
 
-CREATE TABLE `modules` (
-  `module_id` int(12) NOT NULL auto_increment,
-  `module_name` varchar(30) NOT NULL default '',
+CREATE TABLE IF NOT EXISTS `modules` (
+  `module_id` int(12) NOT NULL AUTO_INCREMENT,
+  `module_name` varchar(30) NOT NULL DEFAULT '',
   `module_description` text NOT NULL,
-  `module_active` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`module_id`)
+  `module_active` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`module_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -96,13 +85,13 @@ CREATE TABLE `modules` (
 -- Tabellenstruktur für Tabelle `preferences`
 --
 
-CREATE TABLE `preferences` (
-  `preference_id` int(12) NOT NULL auto_increment,
-  `preference_key` varchar(30) NOT NULL default '',
-  `preference_value` varchar(30) NOT NULL default '',
+CREATE TABLE IF NOT EXISTS `preferences` (
+  `preference_id` int(12) NOT NULL AUTO_INCREMENT,
+  `preference_key` varchar(30) NOT NULL DEFAULT '',
+  `preference_value` varchar(30) NOT NULL DEFAULT '',
   `preference_description` text,
-  `preference_order` int(5) NOT NULL default '0',
-  PRIMARY KEY  (`preference_id`)
+  `preference_order` int(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`preference_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -116,11 +105,11 @@ CREATE TABLE `preferences` (
 -- Tabellenstruktur für Tabelle `preferences_to_users`
 --
 
-CREATE TABLE `preferences_to_users` (
-  `preferencesusers_id` int(12) NOT NULL auto_increment,
-  `preferencesusers_user` int(12) NOT NULL default '0',
-  `preferencesusers_preference` int(12) NOT NULL default '0',
-  PRIMARY KEY  (`preferencesusers_id`),
+CREATE TABLE IF NOT EXISTS `preferences_to_users` (
+  `preferencesusers_id` int(12) NOT NULL AUTO_INCREMENT,
+  `preferencesusers_user` int(12) NOT NULL DEFAULT '0',
+  `preferencesusers_preference` int(12) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`preferencesusers_id`),
   KEY `preferencesusers_user` (`preferencesusers_user`),
   KEY `preferencesusers_preference` (`preferencesusers_preference`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -136,13 +125,13 @@ CREATE TABLE `preferences_to_users` (
 -- Tabellenstruktur für Tabelle `sessiondata`
 --
 
-CREATE TABLE `sessiondata` (
-  `sessiondata_id` varchar(32) character set latin1 NOT NULL,
-  `sessiondata_created` int(11) NOT NULL default '0',
-  `sessiondata_lastupdate` int(11) NOT NULL default '0',
+CREATE TABLE IF NOT EXISTS `sessiondata` (
+  `sessiondata_id` varchar(32) CHARACTER SET latin1 NOT NULL,
+  `sessiondata_created` int(11) NOT NULL DEFAULT '0',
+  `sessiondata_lastupdate` int(11) NOT NULL DEFAULT '0',
   `sessiondata_content` text NOT NULL,
   `sessiondata_ip` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`sessiondata_id`)
+  PRIMARY KEY (`sessiondata_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -156,12 +145,12 @@ CREATE TABLE `sessiondata` (
 -- Tabellenstruktur für Tabelle `templatecache`
 --
 
-CREATE TABLE `templatecache` (
-  `templatecache_id` int(12) NOT NULL auto_increment,
-  `templatecache_name` varchar(128) character set latin1 collate latin1_general_ci NOT NULL,
-  `templatecache_timestamp` varchar(32) character set latin1 collate latin1_general_ci NOT NULL,
-  `templatecache_content` text character set latin1 collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`templatecache_id`)
+CREATE TABLE IF NOT EXISTS `templatecache` (
+  `templatecache_id` int(12) NOT NULL AUTO_INCREMENT,
+  `templatecache_name` varchar(128) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `templatecache_timestamp` varchar(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `templatecache_content` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`templatecache_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -175,13 +164,14 @@ CREATE TABLE `templatecache` (
 -- Tabellenstruktur für Tabelle `trafficlog`
 --
 
-CREATE TABLE `trafficlog` (
-  `trafficlog_id` int(12) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `trafficlog` (
+  `trafficlog_id` int(12) NOT NULL AUTO_INCREMENT,
   `trafficlog_module` int(12) NOT NULL,
   `trafficlog_action` int(12) NOT NULL,
   `trafficlog_user` int(12) NOT NULL,
-  `trafficlog_ip` int(10) unsigned default NULL,
-  PRIMARY KEY  (`trafficlog_id`)
+  `trafficlog_ip` int(10) unsigned DEFAULT NULL,
+  `trafficlog_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`trafficlog_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -195,12 +185,12 @@ CREATE TABLE `trafficlog` (
 -- Tabellenstruktur für Tabelle `trafficlog_parameters`
 --
 
-CREATE TABLE `trafficlog_parameters` (
-  `trafficparameters_id` int(12) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `trafficlog_parameters` (
+  `trafficparameters_id` int(12) NOT NULL AUTO_INCREMENT,
   `trafficparameters_trafficid` int(12) NOT NULL,
-  `trafficparameters_key` varchar(64) character set latin1 collate latin1_general_ci NOT NULL,
-  `trafficparameters_value` text character set latin1 collate latin1_general_ci,
-  PRIMARY KEY  (`trafficparameters_id`)
+  `trafficparameters_key` varchar(64) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `trafficparameters_value` text CHARACTER SET latin1 COLLATE latin1_general_ci,
+  PRIMARY KEY (`trafficparameters_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -214,11 +204,11 @@ CREATE TABLE `trafficlog_parameters` (
 -- Tabellenstruktur für Tabelle `userconfirmation`
 --
 
-CREATE TABLE `userconfirmation` (
-  `userconfirmation_id` int(12) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `userconfirmation` (
+  `userconfirmation_id` int(12) NOT NULL AUTO_INCREMENT,
   `userconfirmation_user` int(12) NOT NULL,
-  `userconfirmation_key` varchar(100) character set latin1 collate latin1_general_ci NOT NULL,
-  PRIMARY KEY  (`userconfirmation_id`)
+  `userconfirmation_key` varchar(100) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`userconfirmation_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -232,20 +222,17 @@ CREATE TABLE `userconfirmation` (
 -- Tabellenstruktur für Tabelle `userdata`
 --
 
-CREATE TABLE `userdata` (
-  `userdata_id` int(12) NOT NULL auto_increment,
-  `userdata_user` int(12) NOT NULL default '0',
-  `userdata_firstname` varchar(100) default NULL,
-  `userdata_lastname` varchar(100) default NULL,
-  `userdata_url` varchar(100) default NULL,
-  `userdata_address1` varchar(100) default NULL,
-  `userdata_address2` varchar(100) default NULL,
-  `userdata_city` varchar(100) default NULL,
-  `userdata_zip` varchar(10) default NULL,
-  `userdata_country` varchar(30) default NULL,
-  `userdata_im` varchar(255) default NULL,
-  `userdata_timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`userdata_id`),
+CREATE TABLE IF NOT EXISTS `userdata` (
+  `userdata_id` int(12) NOT NULL AUTO_INCREMENT,
+  `userdata_user` int(12) NOT NULL DEFAULT '0',
+  `userdata_username` varchar(100) DEFAULT NULL,
+  `userdata_firstname` varchar(100) DEFAULT NULL,
+  `userdata_lastname` varchar(100) DEFAULT NULL,
+  `userdata_sex` varchar(5) DEFAULT NULL,
+  `userdata_locale` varchar(5) DEFAULT NULL,
+  `userdata_url` varchar(255) DEFAULT NULL,
+  `userdata_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userdata_id`),
   KEY `userdata_user` (`userdata_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -260,11 +247,11 @@ CREATE TABLE `userdata` (
 -- Tabellenstruktur für Tabelle `userrights`
 --
 
-CREATE TABLE `userrights` (
-  `userright_id` int(12) NOT NULL auto_increment,
-  `userright_action` int(12) NOT NULL default '0',
-  `userright_user` int(12) NOT NULL default '0',
-  PRIMARY KEY  (`userright_id`),
+CREATE TABLE IF NOT EXISTS `userrights` (
+  `userright_id` int(12) NOT NULL AUTO_INCREMENT,
+  `userright_action` int(12) NOT NULL DEFAULT '0',
+  `userright_user` int(12) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userright_id`),
   KEY `userright_action` (`userright_action`),
   KEY `userright_user` (`userright_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -280,11 +267,11 @@ CREATE TABLE `userrights` (
 -- Tabellenstruktur für Tabelle `userroles`
 --
 
-CREATE TABLE `userroles` (
-  `userrole_id` int(12) NOT NULL auto_increment,
-  `userrole_name` varchar(30) NOT NULL default '',
+CREATE TABLE IF NOT EXISTS `userroles` (
+  `userrole_id` int(12) NOT NULL AUTO_INCREMENT,
+  `userrole_name` varchar(30) NOT NULL DEFAULT '',
   `userrole_description` text,
-  PRIMARY KEY  (`userrole_id`)
+  PRIMARY KEY (`userrole_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
@@ -298,11 +285,11 @@ CREATE TABLE `userroles` (
 -- Tabellenstruktur für Tabelle `userroles_to_actions`
 --
 
-CREATE TABLE `userroles_to_actions` (
-  `userroleaction_id` int(12) NOT NULL auto_increment,
-  `userroleaction_userrole` int(12) NOT NULL default '0',
-  `userroleaction_action` int(12) NOT NULL default '0',
-  PRIMARY KEY  (`userroleaction_id`),
+CREATE TABLE IF NOT EXISTS `userroles_to_actions` (
+  `userroleaction_id` int(12) NOT NULL AUTO_INCREMENT,
+  `userroleaction_userrole` int(12) NOT NULL DEFAULT '0',
+  `userroleaction_action` int(12) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userroleaction_id`),
   KEY `userroleright_userrole` (`userroleaction_userrole`),
   KEY `userroleright_userright` (`userroleaction_action`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -318,11 +305,11 @@ CREATE TABLE `userroles_to_actions` (
 -- Tabellenstruktur für Tabelle `userroles_to_users`
 --
 
-CREATE TABLE `userroles_to_users` (
-  `userroleuser_id` int(12) NOT NULL auto_increment,
-  `userroleuser_userrole` int(12) NOT NULL default '0',
-  `userroleuser_user` int(12) NOT NULL default '0',
-  PRIMARY KEY  (`userroleuser_id`),
+CREATE TABLE IF NOT EXISTS `userroles_to_users` (
+  `userroleuser_id` int(12) NOT NULL AUTO_INCREMENT,
+  `userroleuser_userrole` int(12) NOT NULL DEFAULT '0',
+  `userroleuser_user` int(12) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userroleuser_id`),
   KEY `userroleuser_userrole` (`userroleuser_userrole`),
   KEY `userroleuser_user` (`userroleuser_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -338,14 +325,13 @@ CREATE TABLE `userroles_to_users` (
 -- Tabellenstruktur für Tabelle `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(12) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(12) NOT NULL AUTO_INCREMENT,
   `user_username` varchar(255) NOT NULL,
-  `user_password` varchar(255) NOT NULL default '',
-  `user_key` varchar(64) default NULL,
+  `user_password` varchar(255) NOT NULL DEFAULT '',
+  `user_key` varchar(64) DEFAULT NULL,
   `user_active` tinyint(1) NOT NULL,
-  `user_instance` int(12) NOT NULL default '0',
-  PRIMARY KEY  (`user_id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
