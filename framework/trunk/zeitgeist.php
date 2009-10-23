b7384afa8f3177018498efc7931429d2d89b8357
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-10-23 19:27:10 +0000

    Deleted deprecated / cleaned modules from main zg file

--- framework/trunk/zeitgeist.php
+++ framework/trunk/zeitgeist.php
@@ -54,11 +54,7 @@ require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/dataserver.class.php');
 require_once (ZEITGEIST_ROOTDIRECTORY . 'classes/files.class.php');
 
 // include modules
-require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/staticform/staticform.class.php');
 require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/form/form.class.php');
-require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/facebook/facebook.class.php');
-require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/facebook/facebookuserhandler.class.php');
-require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/shop/shop.class.php');
 
 // include misc functions
 require_once (ZEITGEIST_ROOTDIRECTORY . 'includes/autoloader.include.php');
