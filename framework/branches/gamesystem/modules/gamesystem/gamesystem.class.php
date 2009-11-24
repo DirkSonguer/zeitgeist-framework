820b0e02a677d2d2d39b29387077e640ed29d442
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-11-24 06:49:06 +0000

    More components of the gamesystem

--- framework/branches/gamesystem/modules/gamesystem/gamesystem.class.php
+++ framework/branches/gamesystem/modules/gamesystem/gamesystem.class.php
@@ -13,7 +13,11 @@
  */
 
 // include the gamesystem classes
+require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/gamesystem/gameaction.interface.php');
 require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/gamesystem/gamedata.class.php');
 require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/gamesystem/gamehandler.class.php');
+require_once (ZEITGEIST_ROOTDIRECTORY . 'modules/gamesystem/gameautoloader.include.php');
+
+spl_autoload_register ('zgGameautoloader');
 
 ?>
