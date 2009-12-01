c1ac77ae0f0e7d2762b0994f6d2e8c49a6fedd92
dirk <dirk@046e039e-1113-11df-9f54-17e475596acb>
2009-12-01 21:05:00 +0000

    branched to check out full entitysystem

---
+++ framework/branches/entitysystem/classes/entitysetup.class.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * Zeitgeist Application Framework
+ * http://www.zeitgeist-framework.com
+ *
+ * Entitysetup class
+ *
+ * @copyright http://www.zeitgeist-framework.com
+ * @license http://www.zeitgeist-framework.com/zeitgeist/license.txt
+ *
+ * @package ZEITGEIST
+ * @subpackage ZEITGEIST ENTITYSYSTEM
+ */
+
+defined('ZEITGEIST_ACTIVE') or die();
+
+class zgEntitysetup
+{
+	protected $debug;
+	protected $messages;
+	protected $database;
+
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		$this->debug = zgDebug::init();
+		$this->messages = zgMessages::init();
+
+		$this->database = new zgDatabase();
+		$this->database->connect();
+	}
+
+
+	/**
+	 * Creates a new component
+	 * The name of the new component. This is mostly for debugging reasons
+	 * Returns the id of the new component
+	 *
+	 * @param string $name name of the new component
+	 * @param string $description description of the new component
+	 *
+	 * @return int|boolean
+	 */
+	public function createComponent($name, $description='')
+	{
+		$this->debug->guard();
+
+		$sql = "INSERT INTO components(component_name, component_description) ";
+		$sql .= "VALUES('" . $name . "', '" . $description . "')";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem creating new component: could not insert component into database', 'warning');
+			$this->messages->setMessage('Problem creating new component: could not insert component into database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$componentid = $this->database->insertId();
+		if (!$componentid)
+		{
+			$this->debug->write('Problem creating new component: could not get component id', 'warning');
+			$this->messages->setMessage('Problem creating new component: could not get component id', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$sql = "CREATE TABLE component_". $componentid ." ";
+		$sql .= "(`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY) ENGINE = MYISAM";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem creating new component: could not create component table', 'warning');
+			$this->messages->setMessage('Problem creating new component: could not create component table', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard($componentid);
+		return $componentid;
+	}
+
+
+	/**
+	 * Delete a given component
+	 *
+	 * @param string $component id of the component to delete
+	 *
+	 * @return boolean
+	 */
+	public function deleteComponent($component)
+	{
+		$this->debug->guard();
+
+
+		$sql = "DELETE FROM components WHERE component_id='" . $component . "'";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem deleting component: could not delete component from database', 'warning');
+			$this->messages->setMessage('Problem deleting component: could not delete component from database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$sql = "DROP TABLE component_". $component ." ";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem deleting component: could not delete component table', 'warning');
+			$this->messages->setMessage('Problem deleting component: could not delete component table', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	/**
+	 * Creates a new assemblage
+	 * The name of the new assemblage. This is mostly for debugging reasons
+	 * Returns the id of the new assemblage
+	 *
+	 * @param string $name name of the new assemblage
+	 * @param string $description description of the new assemblage
+	 *
+	 * @return int|boolean
+	 */
+	public function createAssemblage($name, $description='')
+	{
+		$this->debug->guard();
+
+		$sql = "INSERT INTO assemblages(assemblage_name, assemblage_description) ";
+		$sql .= "VALUES('" . $name . "', '" . $description . "')";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem creating new assemblage: could not insert assemblage into database', 'warning');
+			$this->messages->setMessage('Problem creating new assemblage: could not insert assemblage into database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$assemblageid = $this->database->insertId();
+		if (!$assemblageid)
+		{
+			$this->debug->write('Problem creating new assemblage: could not get assemblage id', 'warning');
+			$this->messages->setMessage('Problem creating new assemblage: could not get assemblage id', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard($assemblageid);
+		return $assemblageid;
+	}
+
+
+	/**
+	 * Adds a new component to an assemblage
+	 *
+	 * @param int $component id of the component that should be added to the assemblage
+	 * @param int $assemblage id of the assemblage to add the component to
+	 *
+	 * @return boolean
+	 */
+	public function addComponentToAssemblage($component, $assemblage)
+	{
+		$this->debug->guard();
+
+		$sql = "INSERT INTO assemblage_components(assemblagecomponent_component, assemblagecomponent_assemblage) ";
+		$sql .= "VALUES('" . $component . "', '" . $assemblage . "')";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem adding a new component to an assemblage: could not insert data into database', 'warning');
+			$this->messages->setMessage('Problem adding a new component to an assemblage: could not insert data into database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	/**
+	 * Removes a component from an existing assemblage
+	 *
+	 * @param int $component id of the component that should be removed from the assemblage
+	 * @param int $assemblage id of the assemblage to delete the component from
+	 *
+	 * @return boolean
+	 */
+	public function removeComponentFromAssemblage($component, $assemblage)
+	{
+		$this->debug->guard();
+
+		$sql = "DELETE FROM assemblage_components WHERE ";
+		$sql .= "assemblagecomponent_component='" . $component . "' AND ";
+		$sql .= "assemblagecomponent_assemblage='" . $assemblage . "'";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Problem removing a component from an assemblage: could not delete component data', 'warning');
+			$this->messages->setMessage('Problem removing a component from an assemblage: could not delete component data', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+		
+		$this->debug->unguard(true);
+		return true;
+	}
+
+}
+
+?>
---
+++ framework/branches/entitysystem/modules/gamesystem/gamehandler.class.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Zeitgeist Application Framework
+ * http://www.zeitgeist-framework.com
+ *
+ * Gamehandler class
+ *
+ * @copyright http://www.zeitgeist-framework.com
+ * @license http://www.zeitgeist-framework.com/zeitgeist/license.txt
+ *
+ * @package ZEITGEIST
+ * @subpackage ZEITGEIST GAMEHANDLER
+ */
+
+defined('ZEITGEIST_ACTIVE') or die();
+
+class zgGamehandler
+{
+	protected $debug;
+	protected $messages;
+	protected $database;
+	protected $configuration;
+
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		$this->debug = zgDebug::init();
+		$this->messages = zgMessages::init();
+		$this->configuration = zgConfiguration::init();
+
+		$this->database = new zgDatabase();
+		$this->database->connect();
+	}
+
+
+	/**
+	 * Saves a game event to the event handler
+	 * The time offset defines the time when the event will be triggered
+	 * Note that the timescale has to be defined by the game applicationm
+	 * This can be seconds, ticks, rounds, depending on your game
+	 *
+	 * @param integer $action id of the action that should be executed
+	 * @param integer $parameter the parameter/s of the action
+	 * @param integer $player id of the player that the event concerns
+	 * @param integer $time this is the time when the event should be handled
+	 *
+	 * @return boolean
+	 */
+	public function saveGameevent($action, $parameter, $player, $time=0)
+	{
+		$this->debug->guard();
+
+		$sql  = "INSERT INTO game_events(event_action, event_parameter, event_player, event_time) ";
+		$sql .= "VALUES('" . $action . "', '" . $parameter . "', '" . $player . "', '" . $time . "')";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Could save game event: could not write data into database', 'warning');
+			$this->messages->setMessage('Could save game event: could not write data into database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	/**
+	 * Removes a given race event from the event log
+	 *
+	 * @param integer $event id of the event to remove
+	 *
+	 * @return boolean
+	 */
+	public function removeGameevent($event)
+	{
+		$this->debug->guard();
+
+		$sql  = "DELETE FROM game_events WHERE event_id='" . $event . "'";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Could remove game event: could not remove data from database', 'warning');
+			$this->messages->setMessage('Could remove game event: could not remove data from database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	/**
+	 * This stores a given game event to the eventlog.
+	 * The event will be taken from the event table (and deleted there).
+	 *
+	 * @param integer $event id of the event in the event table
+	 *
+	 * @return boolean
+	 */
+	public function logGameevent($event)
+	{
+		$this->debug->guard();
+
+		$sql = "INSERT INTO game_eventlog(eventlog_action, eventlog_parameter, eventlog_player, eventlog_time) ";
+		$sql .= "SELECT event_action, event_parameter, event_player, event_time FROM game_events WHERE event_id='" . $event . "'";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Could not store game event to log: could not insert event data into database', 'warning');
+			$this->messages->setMessage('Could not store game event to log: could not insert event data into database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$sql = "DELETE FROM game_events WHERE event_id='" . $event . "'";
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Could not store game event to log: could not delete event data from event table', 'warning');
+			$this->messages->setMessage('Could not store game event to log: could not delete event data from event table', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+
+		$this->removeGameevent($event);
+
+		$this->debug->unguard(true);
+		return true;
+	}
+
+
+	/**
+	 * Handles all race events
+	 * The time define the upper limit for the events to be executed
+	 * If a player id is given, only the events for the respective player
+	 * will be executed
+	 *
+	 * @param integer $time the current time
+	 * @param integer $player id of the player to execute events for
+	 * 
+	 * @return boolean
+	 */
+	public function handleGameevents($time, $player=0)
+	{
+		$this->debug->guard();
+
+		// get all events for the active player and the current round
+		$sql = "SELECT ge.event_id, ga.action_class, ge.event_parameter FROM game_events ge ";
+		$sql .= "LEFT JOIN game_actions ga ON ge.event_action = ga.action_id ";
+		$sql .= "WHERE ge.event_time <= '" . $time . "'";
+		if ($player > 0)
+		{
+			$sql .= " AND ge.event_player='" . $player . "'";
+		}
+		$sql .= " ORDER BY ge.event_time ASC";
+
+		$res = $this->database->query($sql);
+		if (!$res)
+		{
+			$this->debug->write('Could not handle game events: could not load events from the database', 'warning');
+			$this->messages->setMessage('Could not handle game events: could not load events from the database', 'warning');
+			$this->debug->unguard(false);
+			return false;
+		}
+		
+		// execute event data
+		while ($event = $this->database->fetchArray($res))
+		{
+			//check if the event exists and what function to call				
+			if ( (empty($event['action_class'])) || (!class_exists($event['action_class'], true)) )
+			{
+				$this->debug->write('Could not handle game event: event class was not found: '.$event['action_class'], 'warning');
+				$this->messages->setMessage('Could not handle game event: event class was not found: '.$event['action_class'], 'warning');
+				$this->debug->unguard(false);
+				return false;
+			}
+
+			// load the module class through the autoloader
+			$eventClass = new $event['action_class'];
+
+			call_user_func(array(&$eventClass, 'execute'), $event['event_parameter'], $time);
+			$this->logGameevent($event['event_id']);
+		}
+
+		$this->debug->unguard(true);
+		return true;		
+	}
+
+}
+
+?>
