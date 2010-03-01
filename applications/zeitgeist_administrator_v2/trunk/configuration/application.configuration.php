88dcf58f5200a553bc37a583c0d2566bc207e0d1
dirk@songuer.de <dirk@songuer.de@046e039e-1113-11df-9f54-17e475596acb>
2010-03-01 23:32:52 +0000

    Further work on the installer for the ZGA

--- applications/zeitgeist_administrator_v2/trunk/configuration/application.configuration.php
+++ applications/zeitgeist_administrator_v2/trunk/configuration/application.configuration.php
@@ -1,9 +1,10 @@
+
 <?php
 
 	define(ZG_DB_DBSERVER, 'localhost');
 	define(ZG_DB_USERNAME, 'root');
 	define(ZG_DB_USERPASS, '');
-	define(ZG_DB_DATABASE, 'zeitgeist_administrator');
+	define(ZG_DB_DATABASE, 'zeitgeist_administrato');
 	define(ZG_DB_CONFIGURATIONCACHE, 'configurationcache');
 
 ?>
