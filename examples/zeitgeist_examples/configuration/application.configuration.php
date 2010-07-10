354378fb9a0f546f418c2698d2bc19ce7c0108ee
dirk@songuer.de <dirk@songuer.de@046e039e-1113-11df-9f54-17e475596acb>
2010-07-10 22:16:54 +0000

    Fixed some issues with the ZG examples

--- examples/zeitgeist_examples/configuration/application.configuration.php
+++ examples/zeitgeist_examples/configuration/application.configuration.php
@@ -3,7 +3,7 @@
 	define(ZG_DB_DBSERVER, 'localhost');
 	define(ZG_DB_USERNAME, 'root');
 	define(ZG_DB_USERPASS, '');
-	define(ZG_DB_DATABASE, 'zeitgeist_administrator');
+	define(ZG_DB_DATABASE, 'zeitgeist_examples');
 	define(ZG_DB_CONFIGURATIONCACHE, 'configurationcache');
 
 ?>
