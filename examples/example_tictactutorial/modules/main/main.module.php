93c6591cfe76b162af386eecb04e48000d0f3442
dirk@songuer.de <dirk@songuer.de@046e039e-1113-11df-9f54-17e475596acb>
2010-08-30 04:45:10 +0000

    TICTACTUTORIAL: added part 4 (presentation layer)

--- examples/example_tictactutorial/modules/main/main.module.php
+++ examples/example_tictactutorial/modules/main/main.module.php
@@ -26,7 +26,24 @@ class main
 	{
 		$this->debug->guard();
 
-        echo "Hello, World!";
+		// create a new template object
+		$tpl = new zgTemplate();
+
+		// load the template file
+		// we're using the template path defined in the application
+		// configuration (/configuration(application.ini), loaded
+		// in the index.php (line 54)
+		// note that the name is static in this example it could
+		// be also defined in the configuration (either for the
+		// application or module)
+		// the load method loads the content of the template file
+		// into the object.
+		$tpl->load($this->configuration->getConfiguration('application', 'application', 'templatepath') . '/main_index.tpl.html');
+
+		// this shows the final template in the browser
+		// all template commands and control codes will be stripped
+		// before output
+		$tpl->show();
 
 		$this->debug->unguard(true);
 		return true;
