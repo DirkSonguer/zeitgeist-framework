4951b24be46ecc78ba7fa36b3cf1d8f9e5444da0
DirkSonguer@gmail.com <DirkSonguer@gmail.com@046e039e-1113-11df-9f54-17e475596acb>
2011-07-14 08:48:53 +0000

    ZG: On second thought: escaping is not needed here anyway

--- framework/trunk/classes/parameters.class.php
+++ framework/trunk/classes/parameters.class.php
@@ -196,38 +196,6 @@ class zgParameters
 			{
 				$safeParameters[ $parametername ] = $this->rawParameters[ $parameterdefinition[ 'source' ] ][ $parametername ];
 
-				// strip slashes
-				if ( ( !empty( $parameterdefinition[ 'stripslashes' ] ) ) && ( $parameterdefinition[ 'stripslashes' ] == 'true' ) )
-				{
-					if ( is_array( $safeParameters[ $parametername ] ) )
-					{
-						foreach ( $safeParameters[ $parametername ] as $key => $value )
-						{
-							$safeParameters[ $parametername ][ $key ] = stripslashes( $value );
-						}
-					}
-					else
-					{
-						$safeParameters[ $parametername ] = stripslashes( $safeParameters[ $parametername ] );
-					}
-				}
-
-				// escape parameter
-				if ( ( !empty( $parameterdefinition[ 'escape' ] ) ) && ( $parameterdefinition[ 'escape' ] == 'true' ) )
-				{
-					if ( is_array( $safeParameters[ $parametername ] ) )
-					{
-						foreach ( $safeParameters[ $parametername ] as $key => $value )
-						{
-							$safeParameters[ $parametername ][ $key ] = mysql_real_escape_string( $value );
-						}
-					}
-					else
-					{
-						$safeParameters[ $parametername ] = mysql_real_escape_string( $safeParameters[ $parametername ] );
-					}
-				}
-
 				unset( $unsafeParameters[ $parametername ] );
 			}
 		}
